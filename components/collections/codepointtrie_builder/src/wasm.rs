863efada00b84ef960b11c98dfc1fbe45b2e81cb
Pawan Dogra <pawan.dogra99@gmail.com>
2023-05-05 13:31:47 +0000

    pass arrays to wasmer

--- components/collections/codepointtrie_builder/src/wasm.rs
+++ components/collections/codepointtrie_builder/src/wasm.rs
@@ -6,8 +6,10 @@ use crate::CodePointTrieBuilder;
 use crate::CodePointTrieBuilderData;
 use icu_collections::codepointtrie::TrieType;
 use icu_collections::codepointtrie::TrieValue;
-use std::io::{Read, Write};
-use wasmer::{Module, Store};
+use std::io::Read;
+use wasmer::TypedFunction;
+use wasmer::WasmPtr;
+use wasmer::{Instance, Module, Store};
 use wasmer_wasix::{Pipe, WasiEnv};
 
 const WASM_BYTES: &[u8] = include_bytes!("../list_to_ucptrie.wasm");
@@ -27,25 +29,94 @@ where
         .to_owned(),
         format!("{}", std::mem::size_of::<T::ULE>() * 8),
     ];
+
+    let trie_type_str = match builder.trie_type {
+        TrieType::Fast => "fast",
+        TrieType::Small => "small",
+    };
+
     let mut store = Store::default();
     let module = Module::new(&store, WASM_BYTES).expect("valid WASM");
 
-    let (mut stdin_sender, stdin_reader) = Pipe::channel();
     let (stdout_sender, mut stdout_reader) = Pipe::channel();
 
+    let mut wasi_env = WasiEnv::builder("list_to_ucptrie")
+        .stdout(Box::new(stdout_sender))
+        .finalize(&mut store)
+        .expect("Unable to create wasi_env");
+
+    // Create the WebAssembly instance with the module and the WasiEnv
+    let import_object = wasi_env
+        .import_object(&mut store, &module)
+        .expect("walid wasm file");
+    let instance = Instance::new(&mut store, &module, &import_object).expect("valid instance");
+
+    // Attach memory export
+    let memory = instance.exports.get_memory("memory").expect("memory");
+    // wasi_env.data_mut(&mut store).set_memory(memory.clone());
+
+    // Initialize WasiEnv
+    wasi_env
+        .initialize(&mut store, instance.clone())
+        .expect("initialize");
+
+    let malloc = instance
+        .exports
+        .get_typed_function::<i32, WasmPtr<u8>>(&mut store, "malloc")
+        .expect("malloc is exported");
+    let trie_type_ptr: WasmPtr<u8> = malloc.call(&mut store, trie_type_str.len() as i32).unwrap();
+
+    let memory_view = memory.view(&store);
+    memory_view
+        .write(trie_type_ptr.offset().into(), trie_type_str.as_bytes())
+        .expect("unable to write 'trie_type_str'");
+
     let CodePointTrieBuilderData::ValuesByCodePoint(values) = builder.data;
-    writeln!(stdin_sender, "{}", values.len()).expect("valid pipe");
-    for value in values {
-        let num: u32 = (*value).into();
-        writeln!(stdin_sender, "{num}").expect("valid pipe");
+    let malloc = instance
+        .exports
+        .get_typed_function::<i32, WasmPtr<u32>>(&mut store, "malloc")
+        .expect("malloc is exported");
+    let base_ptr: WasmPtr<u32> = malloc
+        .call(&mut store, (values.len() * 4) as i32)
+        .expect("Unable to allocate memory for values");
+    let memory_view = memory.view(&store);
+
+    for (i, value) in values.iter().enumerate() {
+        let value_ptr = base_ptr
+            .add_offset(i as u32)
+            .expect(&format!("{} element offset", i));
+        let value_deref_ptr = value_ptr.deref(&memory_view);
+        value_deref_ptr
+            .write((*value).into())
+            .expect("Unable to write value");
     }
 
-    WasiEnv::builder("list_to_ucptrie")
-        .stdin(Box::new(stdin_reader))
-        .stdout(Box::new(stdout_sender))
-        .args(args)
-        .run_with_store(module, &mut store)
-        .expect("run");
+    let construct_ucptrie: TypedFunction<(i32, i32, i32, i32, i32, i32), i32> = instance
+        .exports
+        .get_typed_function(&mut store, "construct_ucptrie")
+        .expect("'construct_ucptrie' is exported");
+
+    let exit_result = construct_ucptrie.call(
+        &mut store,
+        builder.default_value.into() as i32,
+        builder.error_value.into() as i32,
+        trie_type_ptr
+            .offset()
+            .try_into()
+            .expect("trie_type_ptr is valid"),
+        // size_of::<T::ULE>() * 8 fits in i32
+        (std::mem::size_of::<T::ULE>() * 8).try_into().unwrap(),
+        base_ptr.offset().try_into().expect("base ptr is valid"),
+        values.len() as i32,
+    );
+
+    match exit_result {
+        // 0 denotes success,
+        Ok(0) => {}
+        e => panic!("list_to_ucptrie failed in C++: args were: {args:?}: {e:?}"),
+    }
+
+    wasi_env.cleanup(&mut store, None);
 
     // The output is a TOML blob, which we can save in a string
     let mut buf = String::new();
